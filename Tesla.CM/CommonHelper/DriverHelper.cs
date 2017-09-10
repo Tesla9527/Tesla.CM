cd5f4d77c2c6584532e4607de7feca70732c0ce6
Tesla Lau <tesla9527@gmail.com>
2017-09-10 15:00:16 +0800

    Update DriverHelper.cs

--- Tesla.CM/CommonHelper/DriverHelper.cs
+++ Tesla.CM/CommonHelper/DriverHelper.cs
@@ -1,14 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
-using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
 
 namespace Tesla.CM.CommonHelper
 {
